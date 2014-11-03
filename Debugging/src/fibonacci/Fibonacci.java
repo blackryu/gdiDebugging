198aef1a848e08b62df25cb7282c5624a547439d
c.hamilton <cody.hamilton@stud.hs-mannheim.de>
2014-11-03 10:56:48 +0100

    Fibonacci example
    Added extra Fibbonacci method to showcase
    a recursive method.
    
    Signed-off-by: c.hamilton <cody.hamilton@stud.hs-mannheim.de>

--- Debugging/src/fibonacci/Fibonacci.java
+++ Debugging/src/fibonacci/Fibonacci.java
@@ -1,27 +1,43 @@
 package fibonacci;
 
-import java.util.Scanner;
-
+import static gdi.MakeItSimple.*;
 public class Fibonacci {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		
 		int n = 0;
 		do {
 			System.out.print("Enter a number (n>2): ");
-			n = scanner.nextInt();
+			n = readInt();
+			readLine();
 		} while (n < 2);
 
-		printArray(getFiboArray(n));
+		printArray(getFiboArrayIterativ(n));
+		System.out.println();
+		printFibo(n);
 	}
 
-	private static int[] getFiboArray(int n) {
+	private static int[] getFiboArrayIterativ(int n) {
 		int[] f = new int[n];
 		f[0] = 1;
 		f[1] = 1;
 
-		for (int i = 2; i <= n; i++)
+		for (int i = 2; i < n; i++)
 			f[i] = f[i - 1] + f[i - 2];
 		return f;
+	}
+	
+	private static int getFiboRec(int n){
+		if(n == 0 || n == 1){
+			return n;
+		} else {
+			return getFiboRec(n - 1) + getFiboRec(n -2);
+		}
+	}
+	
+	private static void printFibo(int n) {
+		for(int i = 0; i <= n; i++){
+			System.out.printf("Fibbonacci of %d is: %d\n",i, getFiboRec(i));
+		}
 	}
 
 	private static void printArray(int[] fiboArray) {
